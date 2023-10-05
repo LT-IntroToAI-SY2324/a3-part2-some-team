1b6114b183a4514ab033e500e7e7226f154975d1
Jerry Yant <jyantaratan@cps.edu>
2023-10-05 11:28:43 -0500

    basic skeleton (work in progress)

---
+++ guess.py
@@ -0,0 +1,24 @@
+# Variables
+player = 0
+com = 0
+random = 0 # for higher or lower
+guess = 0 # for guess my number
+flip = 0 # for heads or tails
+
+# Start of game
+begin = input('Would you like to play? ')
+if begin == "yes":
+    input('Which game should we play? (Higher or lower, Guess my number, Heads or tails) ')
+    if input == "Higher or lower":
+        
+     return "work in progress"
+
+    if input == "Guess my number":
+       
+     return "work in progress"
+
+    else:
+        print("Lets play heads or tails")
+
+elif begin == "no":
+   print(":(")
